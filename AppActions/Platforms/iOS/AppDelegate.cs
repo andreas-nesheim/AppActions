7a3734791965c15de8eb038e459e4e49ef2bee31
Andreas Aronsen Nesheim <andreas.nesheim@capgemini.com>
2022-03-26 12:14:12 +0100

    asd

--- AppActions/Platforms/iOS/AppDelegate.cs
+++ AppActions/Platforms/iOS/AppDelegate.cs
@@ -9,5 +9,5 @@ public class AppDelegate : MauiUIApplicationDelegate
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 
 	public override void PerformActionForShortcutItem(UIApplication application, UIApplicationShortcutItem shortcutItem, UIOperationHandler completionHandler)
-	=> Platform.PerformActionForShortcutItem(application, shortcutItem, completionHandler);
+		=> Platform.PerformActionForShortcutItem(application, shortcutItem, completionHandler);
 }
